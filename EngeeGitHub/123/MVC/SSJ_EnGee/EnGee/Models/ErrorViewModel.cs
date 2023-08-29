0f87137f666d1b25edf0b9033065e8f47b9aeaa4
shashujie01 <shashujieforcode@gmail.com>
2023-08-29 13:45:48 +0800

    PathSet

--- EngeeGitHub/123/MVC/SSJ_EnGee/EnGee/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EnGee.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- EngeeGitHub/123/MVC/SSJ_EnGee/EnGee/ViewModel/SSJ_CAddToCartViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EnGee.ViewModel;
-
-public class SSJ_CAddToCartViewModel
-{
-    public int txtCount { get; set; }
-    public int txtProductId { get; set; }
-    public int deliveryOption { get; set; }
-
-}
