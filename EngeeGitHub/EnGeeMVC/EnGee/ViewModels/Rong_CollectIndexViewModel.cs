3f6d6f493b5acaece904f9eb1c1cd2d5f5af9277
rong <e31019e@gmail.com>
2023-09-11 20:35:28 +0800

    0911更新許願池的所有部分

--- EngeeGitHub/EnGeeMVC/EnGee/ViewModels/Rong_CollectIndexViewModel.cs
+++ EngeeGitHub/EnGeeMVC/EnGee/ViewModels/Rong_CollectIndexViewModel.cs
@@ -3,6 +3,8 @@
     public class Rong_CollectIndexViewModel
     {
         public int CollectId { get; set; }
+        public bool CollectStatus { get; set; }
+
         public string CollectTitle { get; set; }
         public string CollectStartDate { get; set; }
         public string CollectEndDate { get; set; }
@@ -10,7 +12,11 @@
         public string? CollectImagePath { get; set; }
 
         //  from 會員資料表 取得名稱
-        public string Nickname { get; set; }
+        public string Username { get; set; }
+
+        public int MainCategoryId { get; set; }
+        public int SubcategoryId { get; set; }
+
 
     }
 }
