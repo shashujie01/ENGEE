d82988793a37bfbb8960ef49f92bacbaa71635be
rong <135833215+xinrzL7@users.noreply.github.com>
2023-09-11 20:10:26 +0800

    Revert "0911許願池&捐贈表單CRUD"

--- EngeeGitHub/EnGeeMVC/EnGee/ViewModels/Rong_CollectInfomationViewModel.cs
+++ EngeeGitHub/EnGeeMVC/EnGee/ViewModels/Rong_CollectInfomationViewModel.cs
@@ -5,9 +5,19 @@ namespace EnGee.ViewModels
 {
     public class Rong_CollectInfomationViewModel
     {
+    //    private TCollect _collect;
+    //    public TCollect collect
+    //    {
+    //        get { return _collect; }
+    //        set { _collect = value; }
+    //    }
+    //    public Rong_CollectInfomationViewModel() 
+    //    {
+    //        _collect = new TCollect();
+    //    }
 
         public int CollectId { get; set; }
-        public string Username { get; set; }
+        public string Nickname { get; set; }
         public string CollectTitle { get; set; }
         public string CollectCaption
         { get; set; }
@@ -21,6 +31,9 @@ namespace EnGee.ViewModels
         public string? CollectImagePath { get; set; }
         public string? CollectItemName { get; set; }
         public int? CollectAmount { get; set; }
+
+        //[DisplayName("上架中")]
+        //public bool CollectStatus { get; set; }
 
     }
 }
