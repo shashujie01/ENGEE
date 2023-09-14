2dd1cf299a80617c82fc80e650de0c3125852cfa
shashujie01 <shashujieforcode@gmail.com>
2023-09-15 00:45:28 +0800

    刪除測試頁面，程式碼優化縮短，

--- EngeeGitHub/EnGeeMVC/EnGee/Models/SSJ_CDictionary.cs
+++ EngeeGitHub/EnGeeMVC/EnGee/Models/SSJ_CDictionary.cs
@@ -3,5 +3,5 @@
 public class SSJ_CDictionary
 {
     public static readonly string SK_PURCAHSED_PRODUCTS_LIST = "SK_PURCAHSED_PRODUCTS_LIST";
-    public static readonly string SK_LOGINED_USER = "SK_LOGINED_USER";
+
 }
