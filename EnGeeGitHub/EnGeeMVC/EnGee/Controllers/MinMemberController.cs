d0aadaf194e24630fa023c12b35bcffffe7ffeca
ivy110123 <46495512+ivy110123@users.noreply.github.com>
2023-09-15 10:55:20 +0800

    0915 #更動Min控制器/TMember/CreateView/layout (1)更新註冊表單null狀況時的預設值 (2)layout引用vue cdn

--- EnGeeGitHub/EnGeeMVC/EnGee/Controllers/MinMemberController.cs
+++ EnGeeGitHub/EnGeeMVC/EnGee/Controllers/MinMemberController.cs
@@ -20,6 +20,29 @@ namespace EnGee.Controllers
         [HttpPost]
         public IActionResult Create(TMember tm)
         {
+            //---------------條件限制規則--------------------//
+            if (tm.RegistrationDate == null)
+            {
+                tm.RegistrationDate = DateTime.Now;  //註冊日預設當日
+            }
+            if (tm.Access == null)
+            {
+                if(tm.CharityProof != null)
+                {
+                    tm.Access = 3;  //公益團體權限3
+                }
+                else
+                {
+                    tm.Access = 1; //一般使用者
+                }
+            }
+            if (tm.Point == null)
+            {
+                tm.Point = 0;
+            }
+            //-------------------------------------------//
+
+
             EngeeContext db = new EngeeContext();
 
             db.Add(tm);
