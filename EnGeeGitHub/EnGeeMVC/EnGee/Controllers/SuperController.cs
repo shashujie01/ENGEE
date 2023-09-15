dbb8f5aa88ca8eb7416eb82c96ad73deb846d40a
shashujie01 <shashujieforcode@gmail.com>
2023-09-15 15:08:27 +0800

    123

--- EnGeeGitHub/EnGeeMVC/EnGee/Controllers/SuperController.cs
+++ EnGeeGitHub/EnGeeMVC/EnGee/Controllers/SuperController.cs
@@ -38,5 +38,26 @@ namespace EnGee.Controllers
                 _userViewModel.LoggedInUser = loggedInUser;
             }
         }
+        //public override void OnActionExecuting(ActionExecutingContext context)//SSJ沒寫完
+        //{//登入後重新導向到指定位置
+        //    base.OnActionExecuting(context);
+        //    if (!HttpContext.Session.Keys.Contains(CDictionary.SK_LOINGED_USER))
+        //    {
+        //        HttpContext.Session.SetString("RedirectAction", context.ActionDescriptor.RouteValues["action"]);
+        //        HttpContext.Session.SetString("RedirectController", context.ActionDescriptor.RouteValues["controller"]);
+
+        //        context.Result = new RedirectToRouteResult(new RouteValueDictionary(new
+        //        {
+        //            controller = "Home",
+        //            action = "LoginLayout"
+        //        }));
+        //    }
+        //}
+        //protected bool IsUserLoggedIn()
+        //    //SSJ
+        //    //檢查是否為登入狀態
+        //{
+        //    return HttpContext.Session.Keys.Contains(CDictionary.SK_LOINGED_USER);
+        //}
     }
 }
