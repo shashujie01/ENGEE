fa908589db89439559e177f2f286e99eeda4302a
ivy110123 <46495512+ivy110123@users.noreply.github.com>
2023-09-15 15:45:08 +0800

    09/15_新增註冊會員信箱功能 #變動Min控制器/TMember/Create.cshtml #新增Servicer資料夾(EmailSender/IEmailSender.cs)>>無作用 #變動appsetting.json>>無作用 #變動ViewModels資料夾>>新增EmailDto

---
+++ EnGeeGitHub/EnGeeMVC/EnGee/Services/EmailService/EmailSender.cs
@@ -0,0 +1,46 @@
+﻿using MailKit.Security;
+using MimeKit.Text;
+using MimeKit;
+using MailKit.Net.Smtp;
+using EnGee.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using EnGee.Controllers;
+
+namespace EnGee.Services.EmailService
+{
+    public class EmailSender : IEmailService
+    {
+        //private readonly IConfiguration _config;
+
+        //public EmailSender(IConfiguration config)
+        //{
+        //    _config = config;
+        //}
+
+        public void SendEmail(/*EmailDto request*/)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("engee", "engeegift@gmail.com"));
+            message.To.Add(new MailboxAddress("ivy", "ivy1101238@yahoo.com.tw"));
+            message.Subject = "MailKit Test";
+            message.Body = new TextPart("plain") { Text = "Hi from MailKit!" };
+
+
+
+
+
+            using var smtp = new SmtpClient();
+            //smtp.Connect(_config.GetSection("EmailHost").Value, 587, SecureSocketOptions.StartTls);
+            //localhost測試時ssl加密先關閉
+            smtp.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate("engeegift@gmail.com", "ijaqyfmyvlwpkjui");
+            smtp.Send(message);
+            smtp.Disconnect(true);
+
+           
+        }
+
+
+    }
+
+}
