7aac2cfed22eea681a499febf1f34e05636aaf89
ChiaChiii <135288851+ChiaChiii@users.noreply.github.com>
2023-09-18 18:20:42 +0800

    修改layout使用者登入狀態 修改supercontroller userprofile編輯

--- EnGeeGitHub/EnGeeMVC/EnGee/Controllers/SuperController.cs
+++ EnGeeGitHub/EnGeeMVC/EnGee/Controllers/SuperController.cs
@@ -1,24 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EnGee.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using prjMvcCoreDemo.Models;
+using System.Text.Json;
+
+
 
 namespace EnGee.Controllers
 {
     public class SuperController : Controller
     {
+        private readonly CHI_CUserViewModel _userViewModel;
+
+        public SuperController(CHI_CUserViewModel userViewModel)
+        {
+            _userViewModel = userViewModel;
+        }
         public override void OnActionExecuting(ActionExecutingContext context)
         { // 使用ActionExecutingContext:有繼承SuperController的Controller中的每個Action執行前會被呼叫
           // 調用基類的OnActionExecuting方法
             base.OnActionExecuting(context);
-            // 檢查Session是否包含已登入用戶的鍵值(SK_LOINGED_USER)
-            // 如果不包含，則重定向到"Home"控制器的"LoginLayout"動作
-            if (!HttpContext.Session.Keys.Contains(CDictionary.SK_LOINGED_USER))
+            string userJson = HttpContext.Session.GetString(CDictionary.SK_LOINGED_USER);
+
+            if (string.IsNullOrEmpty(userJson))
             {
+                // 沒登入
                 context.Result = new RedirectToRouteResult(new RouteValueDictionary(new
                 {
                     controller = "Home",
-                    action = "LoginLayout"
+                    action = "Login"
                 }));
+            }
+            else
+            {
+                // 有登入,傳到共用的viewmodel
+                TMember loggedInUser = JsonSerializer.Deserialize<TMember>(userJson);
+                _userViewModel.LoggedInUser = loggedInUser;
             }
         }
     }
