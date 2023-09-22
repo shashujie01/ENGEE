a4b263e4b11226cfd5e6d8fd5afd045c4f82ba2b
ivy110123 <46495512+ivy110123@users.noreply.github.com>
2023-09-23 06:39:15 +0800

    0923救回Program

---
+++ EnGeeGitHub/EnGeeMVC/EnGee/Models/MinFavorModels.cs
@@ -0,0 +1,111 @@
+﻿namespace EnGee.Models
+{
+    public class MinFavorModel : TProduct
+    {
+        private readonly TProduct _product;
+        public MinFavorModel(TProduct product)
+        {
+            _product = product;
+        }
+        public int ProductId                //Min
+        {
+            get { return _product.ProductId; }
+            set { _product.ProductId = value; }
+        }
+
+
+        public string ProductName          //Min
+        {
+            get { return _product.ProductName; }
+            set { _product.ProductName = value; }
+        }
+
+        public int BrandId
+        {
+            get { return _product.BrandId; }
+            set { _product.BrandId = value; }
+        }
+
+        public int MainCategoryId
+        {
+            get { return _product.MainCategoryId; }
+            set { _product.MainCategoryId = value; }
+        }
+
+        public int SubcategoryId
+        {
+            get { return _product.SubcategoryId; }
+            set { _product.SubcategoryId = value; }
+        }
+
+        public string ProductDescribe  //Min
+        {
+            get { return _product.ProductDescribe; }
+            set { _product.ProductDescribe = value; }
+        }
+
+        public string ProductImagePath
+        {
+            get { return _product.ProductImagePath; }
+            set { _product.ProductImagePath = value; }
+        }
+
+        public int ProductUnitPoint
+        {
+            get { return _product.ProductUnitPoint; }
+            set { _product.ProductUnitPoint = value; }
+        }
+
+        public int ProductRemainingQuantity
+        {
+            get { return _product.ProductRemainingQuantity; }
+            set { _product.ProductRemainingQuantity = value; }
+        }
+
+        public DateTime ProductExpirationDate
+        {
+            get { return _product.ProductExpirationDate; }
+            set { _product.ProductExpirationDate = value; }
+        }
+
+        public string ProductUsageStatus
+        {
+            get { return _product.ProductUsageStatus; }
+            set { _product.ProductUsageStatus = value; }
+        }
+
+        public int DonationStatus      //Min
+        {
+            get { return _product.DonationStatus; }
+            set { _product.DonationStatus = value; }
+        }
+
+        public DateTime DateOfSale
+        {
+            get { return _product.DateOfSale; }
+            set { _product.DateOfSale = value; }
+        }
+        public int DeliveryTypeId
+        {
+            get { return _product.DeliveryTypeId; }
+            set { _product.DeliveryTypeId = value; }
+        }
+
+        public int SellerId
+        {
+            get { return _product.SellerId; }
+            set { _product.SellerId = value; }
+        }
+
+        public int ProductSaleStatus
+        {
+            get { return _product.ProductSaleStatus; }
+            set { _product.ProductSaleStatus = value; }
+        }
+
+        public int? boolFavor { get; set; }  //Min 新增 關聯至MinFavorController
+
+
+
+    }
+}
---
+++ EngeeGitHub/EnGeeMVC/EnGee/Program.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using EnGee.Data;
+using EnGee.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using EnGee.CollectService;
+using EnGee.Models;
+using EnGee.Services.EmailService;
+using EnGee.Services;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("EnGeeContextConnection") ?? throw new InvalidOperationException("Connection string 'EnGeeContextConnection' not found.");
+
+builder.Services.AddDbContext<EnGeeContext>(options => options.UseSqlServer(connectionString));
+
+//builder.Services.y<EnGeeUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<EnGeeContext>();
+
+//Min新增
+// 添加 Identity 服務設定AddDefaultIdentit
+builder.Services.AddIdentity<EnGeeUser, IdentityRole>()
+    .AddEntityFrameworkStores< EnGeeContext > ()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddScoped<UserManager<EnGeeUser>>();
+builder.Services.AddScoped<SignInManager<EnGeeUser>>();
+// Rong新增
+builder.Services.AddDbContext<EngeeContext>(options => options.UseSqlServer(connectionString));
+
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.Cookie.Name = "YourAuthCookieName";
+        options.LoginPath = new PathString("/Home/LoginLayout");
+        options.AccessDeniedPath = new PathString("/Home/AccessDenied");
+    });
+
+
+// Add services to the container.AddSession
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddSession();
+
+// Rong新增
+builder.Services.AddSingleton<IHostedService, CollectStatusUpdate>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseSession();
+app.UseAuthorization();
+app.UseAuthentication();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=MinFavor}/{action=FavorList}/{id?}");
+app.MapRazorPages();
+app.Run();
