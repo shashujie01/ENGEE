886d0c36f02e9a1cf3909c4067e9a3e31de19c9f
rong <e31019e@gmail.com>
2023-09-23 11:36:26 +0800

    0923 補丁覆蓋後推送

--- EngeeGitHub/EnGeeMVC/EnGee/Models/TOrder.cs
+++ EngeeGitHub/EnGeeMVC/EnGee/Models/TOrder.cs
@@ -9,15 +9,9 @@ public partial class TOrder
 
     public DateTime OrderDate { get; set; }
 
-    public int DeliveryTypeId { get; set; }
-
-    public string DeliveryAddress { get; set; } = null!;
-
     public int OrderTotalUsagePoints { get; set; }
 
     public int BuyerId { get; set; }
-
-    public int SellerId { get; set; }
 
     public string OrderStatus { get; set; } = null!;
 
@@ -26,14 +20,4 @@ public partial class TOrder
     public string ConvienenNum { get; set; } = null!;
 
     public int DeliveryFee { get; set; }
-
-    public virtual TMember Buyer { get; set; } = null!;
-
-    public virtual TDeliveryType DeliveryType { get; set; } = null!;
-
-    public virtual TMember Seller { get; set; } = null!;
-
-    public virtual ICollection<TOrderDetail> TOrderDetails { get; set; } = new List<TOrderDetail>();
-
-    public virtual ICollection<TRating> TRatings { get; set; } = new List<TRating>();
 }
