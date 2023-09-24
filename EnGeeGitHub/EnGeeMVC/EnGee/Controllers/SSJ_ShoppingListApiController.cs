a27741fcb118e4fbba096726d2eaf71679f4cc23
shashujie01 <shashujieforcode@gmail.com>
2023-09-24 11:45:54 +0800

    新增購物清單頁面與刪除功能

---
+++ EnGeeGitHub/EnGeeMVC/EnGee/Controllers/SSJ_ShoppingListApiController.cs
@@ -0,0 +1,60 @@
+﻿using EnGee.Models;
+using EnGee.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace EnGee.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SSJ_ShoppingListApiController : ControllerBase
+    {
+        private readonly EngeeContext _db;
+
+        public SSJ_ShoppingListApiController(EngeeContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetOrder(int id)
+        {
+            var order = _db.TOrders.Where(o => o.OrderId == id)
+                        .Select(o => new SSJ_ShoppingListOrderViewModel { /*... map properties ...*/ })
+                        .FirstOrDefault();
+
+            var orderDetails = _db.TOrderDetails.Where(od => od.OrderId == id)
+                        .Select(od => new SSJ_ShoppingListOrderDetailViewModel { /*... map properties ...*/ })
+                        .ToList();
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            var result = new SSJ_ShoppingListCombinedViewModel
+            {
+                Orders = new List<SSJ_ShoppingListOrderViewModel> { order },
+                OrderDetails = orderDetails
+            };
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateOrder([FromBody] SSJ_ShoppingListCombinedViewModel model)
+        {
+            if (model == null || model.Orders == null || model.OrderDetails == null)
+            {
+                return BadRequest("Invalid data.");
+            }
+
+            // ... your update logic here ...
+
+            return Ok(new { Message = "Order updated successfully." });
+        }
+    }
+}
+
---
+++ EnGeeGitHub/EnGeeMVC/EnGee/ViewModel/SSJ_ShoppingListOrderViewModel.cs
@@ -0,0 +1,41 @@
+﻿namespace EnGee.ViewModel
+{
+    public class SSJ_ShoppingListOrderViewModel
+    {
+        public int OrderID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string OrderStatus { get; set; }
+        public string OrderStatusDescription
+        {
+            get
+            {
+                return OrderStatus switch
+                {
+                    "1" => "待出貨",
+                    "2" => "待收貨",
+                    "3" => "訂單已完成",
+                    "4" => "不成立",
+                    "5" => "退貨/退款",
+                    _ => "未知",
+                };
+            }
+        }
+        public int OrderCatagory { get; set; }
+        public string OrderCatagoryDescription
+        {
+            get
+            {
+                return OrderCatagory switch
+                {
+                    1 => "購買",
+                    2 => "一點送",
+                };
+            }
+        }
+        public int BuyerID { get; set; }
+        public string BuyerUsername { get; set; }
+        public string ConvienenNum { get; set; }
+        public int DeliveryFee { get; set; }
+        public int OrderTotalUsagePoints { get; set; }
+    }
+}
