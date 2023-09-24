0d9c7cd779fefc058f217e614cd6f368de525e0b
shashujie01 <shashujieforcode@gmail.com>
2023-09-25 04:01:36 +0800

    完成清單明細(一般會員)與訂單管理(管理員)

---
+++ EnGeeGitHub/EnGeeMVC/EnGee/Controllers/HelloApiController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnGee.Controllers
+{
+        [Route("api/[controller]")]
+        [ApiController]
+        public class HelloApiController : ControllerBase
+        {
+            [HttpGet]
+            public IActionResult Get()
+            {
+                return Ok("HELLO");
+            }
+        }
+    
+}
