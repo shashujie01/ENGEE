7b8e79911d694a2d16caa8b8c8ec3e4f0f1de0eb
shashujie01 <shashujieforcode@gmail.com>
2023-09-24 11:45:54 +0800

    新增購物清單頁面與刪除功能

--- EnGeeGitHub/EnGeeMVC/EnGee/ViewModel/SSJ_ShoppingListOrderDetailViewModel.cs
+++ EnGeeGitHub/EnGeeMVC/EnGee/ViewModel/SSJ_ShoppingListOrderDetailViewModel.cs
@@ -1,23 +1,18 @@
 ﻿namespace EnGee.ViewModel
 {
-    public class ShoppingListViewModel
+    public class SSJ_ShoppingListOrderDetailViewModel
     {
         public int OrderID { get; set; }
-        public DateTime OrderDate { get; set; }
-        public int ProdOrderTotalUsagePointsuctDescribe { get; set; }
-        public int BuyerID { get; set; }
-        public string OrderStatus { get; set; }
-        public int OrderCatagory { get; set; }
-        public string ConvienenNum { get; set; }
-        public int DeliveryFee { get; set; }
+        public string ProductName { get; set; }
         public string ProductImagePath { get; set; }
         public int OrderDetailID { get; set; }
         public int ProductID { get; set; }
         public int ProductUnitPoint { get; set; }
         public int OrderQuantity { get; set; }
         public int SellerID { get; set; }
+        public string SellerUsername { get; set; }
         public int DeliveryTypeID { get; set; }
+        public string DeliveryType { get; set; }
         public string DeliveryAddress { get; set; }
-        public int OrderTotalUsagePoints { get; set; }
     }
 }
