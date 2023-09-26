414ee6e4eac03945cc44439294643d8e719c1f4c
alcyone29 <135235016+alcyone29@users.noreply.github.com>
2023-09-26 16:32:52 +0800

    加入最愛

---
+++ EnGeeGitHub/EnGeeMVC/EnGee/Repositories/MemberFavoriteRepository.cs
@@ -0,0 +1,25 @@
+﻿using EnGee.Models;
+using EnGee.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnGee.Repositories
+{
+    public class MemberFavoriteRepository
+    {
+        private readonly EngeeContext dbContext;
+
+        public MemberFavoriteRepository(EngeeContext context)
+        {
+            dbContext = context;
+        }
+
+        public bool IsProductFavoriteForUser(int memberId, int productId)
+        {
+           
+            return dbContext.TMemberFavorites.Any(f => f.Member.MemberId == memberId && f.Product.ProductId == productId && f.AddFavoriteType == true); //Favoritetype=true=1=壹點贈送
+        }
+    }
+}
+
+//要去Program.cs .添加 MemberFavoriteRepository 到依賴注入容器
+//builder.Services.AddScoped<MemberFavoriteRepository>();
