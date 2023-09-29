c0bdbeb58b31eda065dd046b9dbc5a77a4ffe92b
alcyone29 <pudding1229@gmail.com>
2023-09-29 13:31:05 +0800

    我的最愛管理頁面

--- EnGeeGitHub/EnGeeMVC/EnGee/Controllers/FavoriteController.cs
+++ EnGeeGitHub/EnGeeMVC/EnGee/Controllers/FavoriteController.cs
@@ -25,6 +25,26 @@ namespace EnGee.Controllers
 
         }
 
+        [HttpGet]
+        public async Task<IActionResult> ManageFavorites()
+        {
+            string userJson = HttpContext.Session.GetString(CDictionary.SK_LOINGED_USER);
+            if (string.IsNullOrEmpty(userJson))
+            {
+                // 請先登錄
+                return RedirectToAction("Login", "Home"); //請修改為正確的登入控制器名稱
+            }
+
+            TMember loggedInUser = JsonSerializer.Deserialize<TMember>(userJson);
+            var memberId = loggedInUser.MemberId;
+
+            var favorites = await _dbContext.TMemberFavorites
+                                           .Include(f => f.Product)
+                                           .Where(f => f.MemberId == memberId)
+                                           .ToListAsync();
+
+            return View(favorites);
+        }
         [HttpPost]
         public async Task<IActionResult> AddToFavorites(int productId, bool addFavoriteType)
         {
@@ -67,5 +87,40 @@ namespace EnGee.Controllers
                 return Json(new { success = false, message = ex.Message });
             }
         }
+        [HttpPost]
+        public async Task<IActionResult> RemoveFromFavorites(int favoriteId)
+        {
+            try
+            {
+                string userJson = HttpContext.Session.GetString(CDictionary.SK_LOINGED_USER);
+                if (string.IsNullOrEmpty(userJson))
+                {
+                    return RedirectToAction("Login", "Account"); //请根据您的项目中的登录控制器和动作来修改
+                }
+
+                TMember loggedInUser = JsonSerializer.Deserialize<TMember>(userJson);
+                var memberId = loggedInUser.MemberId;
+
+                var favorite = await _dbContext.TMemberFavorites.FindAsync(favoriteId);
+                if (favorite == null || favorite.MemberId != memberId)
+                {
+                    TempData["ErrorMessage"] = "无法找到或删除其他用户的收藏"; // 使用 TempData 传递错误消息
+                    return RedirectToAction("ManageFavorites");
+                }
+
+                _dbContext.TMemberFavorites.Remove(favorite);
+                await _dbContext.SaveChangesAsync();
+
+                TempData["SuccessMessage"] = "已成功从我的最爱中删除"; // 使用 TempData 传递成功消息
+                return RedirectToAction("ManageFavorites");
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message; // 使用 TempData 传递异常消息
+                return RedirectToAction("ManageFavorites");
+            }
+        }
+
+
     }
 }
